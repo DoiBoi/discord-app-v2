a223feb1862d01567e5725942cddf25524b2b353
Jordan <jordanlieu14@gmail.com>
2025-08-15 22:56:51 -0700

    disabling supabase connectivity due to it not working

--- commands/utility/balance.js
+++ commands/utility/balance.js
@@ -1,17 +1,17 @@
-import { createClient } from '@supabase/supabase-js'
+// import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://sercxhohulnwvynvhgzf.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+// const supabaseUrl = 'https://sercxhohulnwvynvhgzf.supabase.co'
+// const supabaseKey = process.env.SUPABASE_KEY
+// const supabase = createClient(supabaseUrl, supabaseKey)
 
-export async function connectToDatabase() {
-    const { data, error } = await supabase.auth.signInWithPassword({
-        email: process.env.SUPABASE_LOGIN,
-        password: process.env.SUPABASE_PASSWORD,
-    })
-    if (error) {
-        console.error('Error connecting to Supabase:', error.message)
-        throw new Error('Database connection failed')
-    }
-    return;
-}
+// export async function connectToDatabase() {
+//     const { data, error } = await supabase.auth.signInWithPassword({
+//         email: process.env.SUPABASE_LOGIN,
+//         password: process.env.SUPABASE_PASSWORD,
+//     })
+//     if (error) {
+//         console.error('Error connecting to Supabase:', error.message)
+//         throw new Error('Database connection failed')
+//     }
+//     return;
+// }
