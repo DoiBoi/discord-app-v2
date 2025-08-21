e05c8b7cdfee4c83c20f3b6611a8ac72e4bfb0fb
Jordan <jordanlieu14@gmail.com>
2025-08-20 21:06:53 -0700

    changed functionality of users

--- commands/lookup/getusers.js
+++ commands/lookup/getusers.js
@@ -5,17 +5,25 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('users')
         .setDescription('Get user accounts')
-        .addUserOption(option =>
+        .addStringOption(option =>
             option.setName('user')
                 .setDescription('The user to get accounts for')
-                .setRequired(true))
+                .setRequired(true)
+                .addChoices(
+                    { name: "felanov", value: "felanov" },
+                    { name: "haohao", value: "haohao" },
+                    { name: "knuckles", value: "knuckles"},
+                    { name: "myself", value: "myself"},
+                    { name: "shehab", value: "shehab"}
+                ))
         .setContexts(InteractionContextType.Guild, InteractionContextType.BotDM, InteractionContextType.PrivateChannel),
     async execute(interaction) {
-        const user = interaction.options.getUser('user');
-        const accounts = await getAccounts(user.id);
+        const user = interaction.options.getString('user');
+        const accounts = await getAccounts(user);
+        console.log(accounts);
         if (!accounts) {
             return interaction.reply({ content: 'No accounts found.', ephemeral: true });
         }
-        return interaction.reply({ content: `Accounts for ${user.username}: \n\`${accounts.join('\` / \`')}\``, ephemeral: true });
+        return interaction.reply({ content: `Accounts for ${user}: \n\`${accounts.join('\` / \`')}\``, ephemeral: true });
     }
 }
