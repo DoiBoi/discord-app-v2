f0acc1168ed070392b2e816342a0908ed3a5fdcc
Jordan <jordanlieu14@gmail.com>
2026-06-11 21:59:26 -0700

    made more progress, need to add components

--- commands/public/exchange.js
+++ commands/public/exchange.js
@@ -1,25 +1,14 @@
 const { SlashCommandBuilder, InteractionContextType, MessageFlags } = require('discord.js');
 const { execute } = require('../balance/subbal');
 
-const send_currency = {
-    bep20: 1,
-    erc20: 1,
-    usdt_sol: 1,
-    usdc_sol: 1,
-    eth: 1,
-    ltc: 1,
-    sol: 1,
-    interac: 1
-}
-
-const rec_currency = {
-    paypal: 0.97,
-    cashapp: 0.97,
-    zelle: 0.97,
-    venmo: 0.97,
-    crypto: 0.98,
-    interac: 0.93,
-    alipayhk: 1
+const rec_rates = {
+    "PayPal": 0.97,
+    "CashApp": 0.97,
+    "Zelle": 0.97,
+    "Venmo": 0.97,
+    "Crypto": 0.98,
+    "Interac": 0.93,
+    "Alipay HK": 1
 }
 
 module.exports = {
@@ -28,31 +17,31 @@ module.exports = {
         .setDescription("INSERT DESCRIPTION HERE")
         .setContexts(InteractionContextType.Guild, InteractionContextType.BotDM, InteractionContextType.PrivateChannel)
         .addStringOption(option =>
-            option.setName("sending_cur")
+            option.setName("sending_curr")
                 .setDescription("The currency you're looking to send in")
                 .setRequired(true)
                 .addChoices(
-                    { name: "USDT BEP20", value: "bep20" },
-                    { name: "USDT ERC20", value: "erc20" },
-                    { name: "USDT SOL", value: "usdt_sol" },
-                    { name: "USDC SOL", value: "usdc_sol" },
-                    { name: "ETH", value: "eth" },
+                    { name: "USDT BEP20", value: "USDT BEP20" },
+                    { name: "USDT ERC20", value: "USDT ERC20" },
+                    { name: "USDT SOL", value: "USDT SOL" },
+                    { name: "USDC SOL", value: "USDC SOL" },
+                    { name: "ETH", value: "ETH" },
                     { name: "LTC", value: "LTC" },
-                    { name: "SOL", value: "sol" },
-                    { name: "Interac", value: "interac" }
+                    { name: "SOL", value: "SOL" },
+                    { name: "Interac", value: "Interac" }
                 )
         )
         .addStringOption(option =>
             option.setName("recieving_curr")
                 .setDescription("The currency you're looking to recieve in")
                 .addChoices(
-                    { name: "PayPal", value: "paypal" },
-                    { name: "CashApp", value: "cashapp" },
-                    { name: "Zelle", value: "zelle" },
-                    { name: "Venmo", value: "venmo" },
-                    { name: "Crypto", value: "crypto" },
-                    { name: "Interac", value: "interac" },
-                    { name: "Alipay HK", value: "alipayhk" }
+                    { name: "PayPal", value: "PayPal" },
+                    { name: "CashApp", value: "CashApp" },
+                    { name: "Zelle", value: "Zelle" },
+                    { name: "Venmo", value: "Venmo" },
+                    { name: "Crypto", value: "Crypto" },
+                    { name: "Interac", value: "Interac" },
+                    { name: "Alipay HK", value: "Alipay HK" }
                 )
         )
         .addNumberOption(option =>
@@ -85,10 +74,16 @@ module.exports = {
             })
             return
         }
-        let message = ""
+        let message = "<a:7loading:1210687656703561758>";
+        const multiplier = rec_rates[recieving_curr];
         if (sending_amt) {
-
+            message += `You will recieve **\$${(sending_amt * multiplier).toFixed(2)} ${recieving_curr}** when sending **\$${sending_amt} ${sending_curr}**`
+        } 
+        if (recieving_amt) {
+           message += `You must send **\$${(recieving_amt / multiplier).toFixed(2)} ${sending_curr}** to recieve **\$${recieving_amt} ${recieving_curr}**`
         }
+
+        await interaction.reply({ content: message });
     }
 
 }
