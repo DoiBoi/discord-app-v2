ec986b4a45099f21d9c459e9c865198702ed481f
Jordan <jordanlieu14@gmail.com>
2026-07-13 18:50:59 -0700

    pull

--- commands/public/tempTrigger.js
+++ commands/public/tempTrigger.js
@@ -179,7 +179,7 @@ module.exports = {
     const fee = interaction.options.getNumber("fee");
     const min = interaction.options.getNumber("min");
     const fnf = interaction.options.getBoolean("fnf");
-    
+
     await interaction.deferReply({ ephemeral: true });
     if (recieving == "PayPal" && fnf === null) {
       return await interaction.reply({
@@ -238,4 +238,5 @@ module.exports = {
   buildDropdown,
   buildMessage,
   buildResponse,
+  ORDER
 };
