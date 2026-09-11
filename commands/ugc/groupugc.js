07730766a063cb3d6cea3be71c9f3143c7e45dbc
Jordan <jordanlieu14@gmail.com>
2026-09-11 12:38:36 -0700

    create stub functions

---
+++ commands/ugc/groupugc.js
@@ -0,0 +1,22 @@
+const {
+  SlashCommandBuilder,
+  MessageFlags,
+  InteractionContextType,
+} = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("groupugc")
+    .setDescription("")
+    .setContexts(
+      InteractionContextType.Guild,
+      InteractionContextType.BotDM,
+      InteractionContextType.PrivateChannel,
+    ),
+  async execute(interaction) {
+    await interaction.reply({
+      content: "In development",
+      flags: MessageFlags.Ephemeral,
+    });
+  },
+};
