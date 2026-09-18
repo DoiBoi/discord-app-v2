9503b53c50ce9812298c887a4edf8542e37332a4
Jordan <jordanlieu14@gmail.com>
2026-09-17 17:23:11 -0700

    added active temp command

--- commands/public/getActiveTemp.js
+++ commands/public/getActiveTemp.js
@@ -13,15 +13,17 @@ const MESSAGES_TABLE = ids.message_link;
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("gettemp")
-    .setDescription("Shows channels with a temp entry")
+    .setName("getactivetemp")
+    .setDescription("Shows channels with active temp entries")
     .setContexts(
       InteractionContextType.Guild,
       InteractionContextType.BotDM,
       InteractionContextType.PrivateChannel,
     ),
   async execute(interaction) {
-    const exchanges = Object.values(await getExchanges()).flat();
+    const exchanges = Object.values(await getExchanges())
+      .flat()
+      .filter((item) => item[MESSAGES_TABLE].length > 0 || item.pending > 0);
     if (exchanges.length <= 0) {
       return await interaction.reply({
         content: "There are no active transactions!",
@@ -29,9 +31,18 @@ module.exports = {
       });
     }
     const message = exchanges.reduce((acc, exchange) => {
-      acc += `<#${exchange.channel}> \`${exchange.info}\`: \$${exchange.pending > 0 ? `${exchange.amount}-${exchange.pending}=${(exchange.amount - exchange.pending).toFixed(2)}` : exchange.amount.toFixed(2)}\n`;
+      acc += `<#${exchange.channel}> \`${exchange.info}\`: \$${exchange.pending > 0 ? `${exchange.amount}-${exchange.pending}=${(exchange.amount - exchange.pending).toFixed(2)}` : exchange.amount.toFixed(2)}${exchange[
+        MESSAGES_TABLE
+      ].reduce((acc, message, idx, arr) => {
+        if (idx === arr.length - 1) {
+          acc += `${message.url}`;
+        } else {
+          acc += `${message.url}, `;
+        }
+        return acc;
+      }, " ")}\n`;
       return acc;
-    }, "Here the the current temps:\n");
+    }, "Here the the current active exchanges:\n");
     await interaction.reply({
       content: message,
       flags: MessageFlags.Ephemeral,
